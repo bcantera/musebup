baa2ec51b02f1d3aad0a02a0cb2c3a4ca4b133a7
bcanterac <45189283+bcanterac@users.noreply.github.com>
2018-11-24 02:03:08 -0300

    Update eepromRead_I2C.py
    Added bus pirate i2c connection method

--- eepromRead_I2C.py
+++ eepromRead_I2C.py
@@ -1,1 +1,20 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
+
+import serial
+
+#Open Bus Pirate serial connection
+ser = serial.Serial('/dev/ttyACM0')
+
+#Setup Bus Pirate for i2c
+ser.write('\n')
+ser.write('\n')
+ser.write('m4\n')
+ser.write('2\n')
+ser.write('2\n')
+ser.write('W\n')
+ser.write('P\n')
+
+line = ser.read(100)
+print line
+
+ser.close()
