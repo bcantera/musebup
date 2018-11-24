c3add9ce7599977c8c1d93f66c0dec4622dc92f8
bcanterac <45189283+bcanterac@users.noreply.github.com>
2018-11-24 10:13:54 -0300

    Update eepromRead_I2C.py
    Added bus pirate mode check and eeprom address read

--- eepromRead_I2C.py
+++ eepromRead_I2C.py
@@ -5,16 +5,23 @@ import serial
 #Open Bus Pirate serial connection
 ser = serial.Serial('/dev/ttyACM0')
 
-#Setup Bus Pirate for i2c
+#Read current mode
 ser.write('\n')
-ser.write('\n')
-ser.write('m4\n')
-ser.write('2\n')
-ser.write('2\n')
-ser.write('W\n')
-ser.write('P\n')
+mode = ser.read(5)
 
-line = ser.read(100)
+#Setup Bus Pirate for i2c
+if mode != 'I2C':
+   ser.write('\n')
+   ser.write('\n')
+   ser.write('m4\n')
+   ser.write('2\n')
+   ser.write('2\n')
+   ser.write('W\n')
+   ser.write('P\n')
+
+#Read address and print it (redirect to file with "command >> output.log)"
+ser.write('[0xa1 r:256]\n')
+line = ser.readline()
 print line
 
 ser.close()
