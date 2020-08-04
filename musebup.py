9af3fd8947eb1792e19ae8cd59975ec62631e1cc
Braihan Cantera <bcanterac@gmail.com>
2020-08-04 02:18:07 -0300

    Improved serial read with decode

--- musebup.py
+++ musebup.py
@@ -3,7 +3,6 @@
 import os
 import time
 import serial
-import binascii
 
 #Check for execution privileges
 def privCheck():
@@ -14,10 +13,25 @@ def privCheck():
        isAllowed = True
     return isAllowed
 
+#Check for previous dump.hex
+def dumpCheck():
+
+    if os.path.isfile('dump.hex') == True:
+        checking = True
+        while checking:
+            confirm_delete = input("File dump.hex found, delete it? (y/n)\n")
+            if confirm_delete == "y":
+                os.remove("dump.hex")
+                checking = False
+            elif confirm_delete == "n":
+                print ("Appending to actual dump.hex")
+                checking = False
+            else:
+                print ("\n### Incorrect option, please type \"y\" or \"n\" \n")
+
 #Menu design
 def menu():
    menu = True
-
    while menu and privCheck:
        print (31 * "-" , "MENU" , 31 * "-")
        print ("1. Dump EEPROM")
@@ -25,7 +39,7 @@ def menu():
        print ("3. Exit")
        print (68 * "-")
 
-       option = raw_input()
+       option = input()
        if option == '1':
           menu = False
        elif option == '2':
@@ -42,7 +56,7 @@ def printOffsets():
    out = ''
    palabra = ''
    while ser.inWaiting() > 0:
-     out += ser.read(1)
+     out += ser.read(1).decode()
    if out != '':
      out = out.split("\n",7)[7];
      out = out.split("I2C STOP BIT",1)[0];
@@ -54,7 +68,6 @@ def printOffsets():
      print (out + '\n')
 
      #Saving output to file
-     os.remove("dump.hex")
      f = open( 'dump.hex', 'a' )
      f.write( '\n' + out + '\n' )
      f.close()
@@ -67,8 +80,12 @@ def printOffsets():
 
 checkPermission = privCheck()
 checkBusPirate = os.path.exists('/dev/ttyACM0')
+checkDump = dumpCheck()
 
-if checkBusPirate and checkPermission and menu() == '1':
+if checkBusPirate == False and checkPermission :
+   print ('### Bus pirate not found')
+
+elif checkBusPirate and checkPermission and menu() == '1':
 
    #Open Bus Pirate serial connection and clear previous input/output buffers
    ser = serial.Serial('/dev/ttyACM0', baudrate=115200, bytesize=8, parity='N', stopbits=1)
@@ -76,35 +93,38 @@ if checkBusPirate and checkPermission and menu() == '1':
    ser.reset_output_buffer()
 
    #Read current mode
-   ser.write('\n')
-   mode = ser.read(6)
-   mode = mode.replace('\n','')
-   mode = mode.replace(' ','')
+   ser.write(b'\n')
+   mode = str(ser.read(6))
+   mode = mode.replace('\'','')
+   mode = mode.replace('b\\r\\n','')
 
    #Setup Bus Pirate for I2C
-   if mode.encode("hex") != '0d4932433e':
-      ser.write('\n')
-      ser.write('\n')
-      ser.write('\n')
-      ser.write('m4\n')
-      ser.write('2\n')
-      ser.write('2\n')
-      ser.write('W\n')
-      ser.write('P\n')
+   if mode != 'I2C>':
+      ser.write(b'\n')
+      ser.write(b'\n')
+      ser.write(b'\n')
+      ser.write(b'm4\n')
+      ser.write(b'2\n')
+      ser.write(b'2\n')
+      ser.write(b'W\n')
+      ser.write(b'P\n')
       time.sleep(0.5)
       ser.reset_input_buffer()
       ser.reset_output_buffer()
 
    #Search for available memory addresses
-   ser.write('(1)\n')
+   ser.write(b'(1)\n')
    time.sleep(0.5)
    out = ''
-   while ser.inWaiting() > 0:
-      out += ser.read(1)
-      out = out.replace("\n", "")
-      out = out.replace("(1)", "")
-      out = out.replace("Searching I2C address space. Found devices at:", "")
-      out = out.replace("I2C>", "")
+
+   # Read (1) from buffer
+   out = (ser.readline()).decode()
+
+   # Read "Searching I2C address space. Found devices at:" from buffer
+   out = (ser.readline()).decode()
+
+   # Read actual addresses
+   out = (ser.readline()).decode()
 
    if "Warning: *Short or no pull-up" in out:
       print ("\n### Message from Bus pirate:\n" + out + "\n")
@@ -137,7 +157,7 @@ if checkBusPirate and checkPermission and menu() == '1':
       y = offsetsR.index(min(offsetsR))
       for y in range(0, offsetsR.index(max(offsetsR))+1):
         readOffset = '[' + offsetsW[y] + ' 0x00][' + offsetsR[y] + ' r:1024]\n'
-        ser.write(readOffset)
+        ser.write(readOffset.encode())
         time.sleep(0.5)
         print (offsetsR[y] + ':')
         printOffsets()
@@ -146,56 +166,3 @@ if checkBusPirate and checkPermission and menu() == '1':
       print ('### No addresses found')
 
    ser.close()
-
-elif checkBusPirate == False and checkPermission :
-   print ('### Bus pirate not found')
-
-
-#Search for available memory addresses
-ser.write('(1)\n')
-time.sleep(0.5)
-out = ''
-while ser.inWaiting() > 0:
-   out += ser.read(1)
-   out = out.replace("\n", "")
-   out = out.replace("(1)", "")
-   out = out.replace("Searching I2C address space. Found devices at:", "")
-   out = out.replace("I2C>", "")
-
-#If any address is found, print it
-if '0x' in out:
-   print ('Found addresses:\n' + out + '\n')
-   offsetsW = out.split(") ")
-   offsetsR = out.split(") ")
-
-   #Build W addresses array
-   y = offsetsW.index(max(offsetsW))
-   while y != -3:
-      offsetsW[y+1] = offsetsW[y+1][:-7]
-      del offsetsW[y]
-      y = y - 2
-
-   #Build R addresses array
-   y = offsetsR.index(max(offsetsR))
-   while y != -3:
-      del offsetsR[y+1]
-      y = y - 2
-
-   y = offsetsR.index(max(offsetsR))
-   while y != -1:
-      offsetsR[y] = offsetsR[y][:-7]
-      y = y -1
-
-   #Read and print offsets
-   y = offsetsR.index(min(offsetsR))
-   for y in range(0, offsetsR.index(max(offsetsR))+1):
-      readOffset = '[' + offsetsW[y] + ' 0x00][' + offsetsR[y] + ' r:1024]\n'
-      ser.write(readOffset)
-      time.sleep(0.5)
-      print (offsetsR[y] + ':')
-      printOffsets()
-
-else:
-    print ('No addresses found')
-
-ser.close()
